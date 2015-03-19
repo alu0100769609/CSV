e7ae0ad340003a9fefe092ec05afab80002fc2b4
Adexe Sabina <alu0100769609@ull.edu.es>
2015-03-19 00:33:13 +0000

    merge con prueba

---
+++ tests/tests.js
@@ -0,0 +1,32 @@
+var assert = chai.assert;
+
+suite('CSV', function() {
+    setup(function(){
+      if (typeof __html__ !== 'undefined') {
+          document.body.innerHTML = __html__['tests/index.html'];
+          original = document.getElementById('original');
+          finaltable = document.getElementById('finaltable');
+      }
+    });
+
+    test('No se separa por comas entre comillas.', function() {
+        original.value = '"esto,es,un,unico,valor","este es otro"';
+        calculate();
+        assert.deepEqual(finaltable.innerHTML, ["esto,es,un,unico,valor","este es otro"]);
+    });
+    test('un valor', function() {
+        original.value = '"esto,es,un,unico,valor"';
+        calculate();
+        assert.deepEqual(finaltable.innerHTML, "esto,es,un,unico,valor");
+    });
+    test('45C = 113F', function() {
+        original.value = "45C";
+        calculate();
+        assert.deepEqual(finaltable.innerHTML, "113.0F");
+    });
+    test('5X = error', function() {
+        original.value = "5X";
+        calculate();
+        assert.match(finaltable.innerHTML, /Try something like '-4.2C'/);
+    });
+});
